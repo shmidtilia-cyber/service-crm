b0f7b6e78be3894c8234eed2916251974de67449
shmidtilia-cyber <shmidt.ilia@gmail.com>
2026-05-28 19:14:14 +0300

    Add company dashboard route

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from crm.views import dashboard
+from crm.views import company_dashboard, dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
+    path('company/', company_dashboard, name='company_dashboard'),
 ]
