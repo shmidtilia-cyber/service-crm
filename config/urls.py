910afe87be1f014f849f7f3660c9fd6320ee2b26
shmidtilia-cyber <shmidt.ilia@gmail.com>
2026-05-28 13:02:30 +0300

    Add Django urls

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from crm.views import dashboard
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', dashboard, name='dashboard'),
+]
