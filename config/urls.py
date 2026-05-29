466712f37724de507784dd08766a6fa93b3bccd8
shmidtilia-cyber <shmidt.ilia@gmail.com>
2026-05-29 10:44:02 +0300

    Add users settings route

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from crm.views import company_dashboard, dashboard
+from crm.views import company_dashboard, dashboard, users_settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
     path('company/', company_dashboard, name='company_dashboard'),
+    path('settings/users/', users_settings, name='users_settings'),
 ]
