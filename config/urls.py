3e1c14a7680d053284d68bf5de43593c1281d035
shmidtilia-cyber <shmidt.ilia@gmail.com>
2026-05-29 15:31:07 +0300

    Add finance route

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from crm.views import company_dashboard, dashboard, users_settings
+from crm.finance_views import finance_dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
     path('company/', company_dashboard, name='company_dashboard'),
+    path('finance/', finance_dashboard, name='finance_dashboard'),
     path('settings/users/', users_settings, name='users_settings'),
 ]
